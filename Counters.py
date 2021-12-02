de5e8e9d0d95593cbfcea291eb696b121f5e3430
kowusek <kowusek@gmail.com>
2021-12-02 15:45:56 +0100

    finished TimeCounter class

--- Counters.py
+++ Counters.py
@@ -1,8 +1,28 @@
+from PySide2.QtWidgets import QApplication, QWidget, QLCDNumber
+from PySide2.QtCore import QTimer
+import sys
+
 class TimeCounter:
 
-    def __init__(self, display) -> None:
+    def __init__(self, display: QLCDNumber) -> None:
+        self.count = 0
         self.display = display
+        self.timer = QTimer(self.display)
+        self.timer.timeout.connect(self.showTime)
+        self.timer.start(1000)
+        self.showTime()
 
-    def resetTime():
-        pass
+    def resetTime(self):
+        self.count = 0
+        self.display.display(self.count)
 
+    def showTime(self):
+        self.display.display(self.count)
+        self.count += 1
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = QLCDNumber()
+    timer = TimeCounter(widget)
+    widget.show()
+    sys.exit(app.exec_())
---
+++ MinesweeperDelegate.py
@@ -0,0 +1,4 @@
+from PySide2.QtWidgets import QStyledItemDelegate
+
+class MinesweeperDelegate(QStyledItemDelegate):
+    pass
